49fb681f2da11314566367592e5d2fca9d3843a8
Ivan Escobar <ivnescbr1989@gmail.com>
2024-04-16 08:00:33 -0400

    setting up for front and backend deployment

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,6 @@
 import express, { Express } from "express";
 import { db } from "./config/db";
-import cors from 'cors';
+// import cors from 'cors';
 // import router from ''
 
 const app: Express = express(); //initalize expre
@@ -10,15 +10,18 @@ const env = process.env.NODE_ENV === 'production' ? 'production' : 'development'
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const corsOptions: cors.CorsOptions = {
-    credentials: true,
-    origin: env === 'development' ? ['http://localhost:5173'] : ['https://tomatoduck-v2.vercel.app']
-  };
+// const corsOptions: cors.CorsOptions = {
+//     credentials: true,
+//     origin: env === 'development' ? ['http://localhost:5173'] : ['https://tomato-duck-v2.vercel.app']
+//   };
   
-  app.use(cors(corsOptions));
+//   app.use(cors(corsOptions));
 
 //   app.use(router)
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
 
-  db.once('on', () => 
+  db.once('open', () => 
     app.listen(PORT, () => console.log(`Server Running! Port: ${PORT} `))
 )
